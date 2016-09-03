971eaeb8bbea62acdc6f46118e77eaa06c0113b5
roodoodey <mathieugrettir@hotmail.com>
2016-09-03 13:48:33 +0000

    Added a lot of the fonts we often use in our application, these were added for our new application league manager. All headers and implementations are yet to be created for the different fonts. They have all been added to the info.plist file.

---
+++ ReusableComponentsApp/UIColor+Exo2Font.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Exo2Font.h
+//  ReusableComponentsApp
+//
+//  Created by Mathieu Skulason on 03/09/16.
+//  Copyright © 2016 Konta ehf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Exo2Font)
+
+@end
---
+++ ReusableComponentsApp/UIColor+Exo2Font.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+Exo2Font.m
+//  ReusableComponentsApp
+//
+//  Created by Mathieu Skulason on 03/09/16.
+//  Copyright © 2016 Konta ehf. All rights reserved.
+//
+
+#import "UIColor+Exo2Font.h"
+
+@implementation UIColor (Exo2Font)
+
+@end
